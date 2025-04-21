c2c9984f654077fc24f918cfde71def4439697a8
sai-45-kla <alkagangula1999@gmail.com>
2025-04-21 20:14:41 +0530

    Update urls.py

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('demo/', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
